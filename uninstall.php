40e31b25044fe09e164abf8f50e5491ea0ff4df5
Eric King <eric.king@lonelyplanet.com>
2014-06-25 16:46:50 -0500

    Updated the list of options to delete

--- uninstall.php
+++ uninstall.php
@@ -5,12 +5,11 @@ if ( !defined('WP_UNINSTALL_PLUGIN'))
 wp_clear_scheduled_hook('rizzo-cron');
 
 $options = array(
-    'rizzo',
+    'rizzo-api',
+    'rizzo-cron',
+    'rizzo-theme-hooks',
     'rizzo-cron-last-run',
     'rizzo-cron-run-count',
-    'rizzo_html_head',
-    'rizzo_html_body',
-    'rizzo_html_footer',
     'rizzo_html_head-endpoint',
     'rizzo_html_body-endpoint',
     'rizzo_html_footer-endpoint',
