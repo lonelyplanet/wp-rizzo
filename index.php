55906a16d7ba35039373621f69afa26a956a7f15
Eric King <eric.king@lonelyplanet.com>
2014-09-10 11:59:29 -0500

    Updated version number

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Plugin URI: http://lonelyplanet.com/
 Author: Eric King
 Author URI: http://webdeveric.com/
 Description: This plugin fetches the four HTML chunks provided by Rizzo and then automatically inserts them into the theme output.
-Version: 0.4.5
+Version: 0.4.6
 */
 
 defined('ABSPATH') || exit;
@@ -30,6 +30,6 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 }
 
 define('WP_RIZZO_FILE', __FILE__);
-define('WP_RIZZO_VERSION', '0.4.5');
+define('WP_RIZZO_VERSION', '0.4.6');
 
 include dirname(__FILE__) . '/wp-rizzo.php';
